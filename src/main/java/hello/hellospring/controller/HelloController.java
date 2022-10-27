9d04d15d5f587407ca3a8a08eb8f7dfff09aa50d
Griotold <gotjd9773@naver.com>
2022-10-27 14:54:25 +0900

    API + Responsebody

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,32 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name", required = false)String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // HTTP body부분에 return 값으로 문자열을 반환해준다.
+    public String helloString(@RequestParam("name")String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello hellApi(@RequestParam("name")String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+    // 컨트롤러 안에 클래스를 선언해줄 수 있다. 자바에서 제공해주는 문법.
+    static class Hello {
+
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+
     }
 }
