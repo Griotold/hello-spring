6f07529e0fe3595be4090a0360171829864b1ec9
Griotold <gotjd9773@naver.com>
2022-10-29 12:42:52 +0900

    회원서비스 개발, 회원가입+전체회원조회+단건조회

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,48 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+
+    public Long join(Member member) {
+        // 리팩토링
+        // 같은 이름을 가진 회원X 
+        // 변수 추출: 컨트롤 + 알트 + v
+        // 메소드 추출: 컨트롤 + 알트 + m
+        validateDuplicateMember(member);
+
+        // 로직은 엄청 쉽다.
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+    /**
+     * 전체 회원 조회
+     * */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    /**
+     * id로 단건 조회
+     * */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
