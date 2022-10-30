1410241c2deb7e88cafad7c31f463d57a82f6817
Griotold <gotjd9773@naver.com>
2022-10-30 13:26:41 +0900

    스프링 빈 의존관계 1. 컴포넌트 스캔방식

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,15 +3,20 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     // 회원 서비스에 리파지터리를 DI 방식으로 변경
     private final MemberRepository memberRepository;
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
+
         this.memberRepository = memberRepository;
     }
 
